05286f2ca766adec82614860be9a3c0cdd69f5ad
doyousketch2 <doyousketch2@yahoo.com>
2013-04-07 06:42:08 -0300

    Add light black background to chat text
    Slightly shaded so it's easier to read.

--- src/game.cpp
+++ src/game.cpp
@@ -1323,6 +1323,11 @@ void the_game(
 			core::rect<s32>(0,0,0,0),
 			//false, false); // Disable word wrap as of now
 			false, true);
+	// Slightly shaded so it's easier to read.
+	// I'd like to set the shadow to the width of the text.
+	// Irrlicht IGUIStaticText has getTextWidth,
+	// but it seems to have no effect on the core::rect above.
+	guitext_chat->setBackgroundColor(video::SColor(80,0,0,0));
 	// Remove stale "recent" chat messages from previous connections
 	chat_backend.clearRecentChat();
 	// Chat backend and console
@@ -3051,7 +3056,7 @@ void the_game(
 			core::rect<s32> rect(
 				10,
 				chat_y,
-				screensize.X - 10,
+				MYMIN(screensize.X - 10 ,font->getDimension(recent_chat.c_str()).Width + 10),
 				chat_y + guitext_chat->getTextHeight()
 			);
 			guitext_chat->setRelativePosition(rect);
